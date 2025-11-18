a83fe34e418e6134e82d5d9a46c1d0d115713b51
hwanga <hwanga555@gmail.com>
2025-11-18 16:30:41 +0900

    [refactor] CRUD useState -> useReducer로 변경

--- src/study/todoApp/App.jsx
+++ src/study/todoApp/App.jsx
@@ -1,4 +1,4 @@
-import { React, useState, useEffect, useRef } from "react";
+import { React, useState, useEffect, useRef, useReducer } from "react";
 import { Header } from "./Header";
 import { TodoEditor } from "./TodoEditor";
 import { TodoList } from "./TodoList";
@@ -25,41 +25,62 @@ const mockTodo = [
   },
 ];
 
-function App() {
-  const idRef = useRef(3);
+function reducer(state, action) {
+  // 상태 변화 코드
+  switch (action.type) {
+    case "CREATE":
+      return [...state, action.newItem];
+    case "UPDATE":
+      return state.map((item) => {
+        return item.id === action.targetId
+          ? { ...item, isDone: !item.isDone }
+          : item;
+      });
+    case "DELETE":
+      return state.filter((item) => item.id !== action.targetId);
+    default:
+      return state;
+  }
+}
 
-  const [todo, setTodo] = useState(mockTodo);
+function App() {
+  const [todo, dispatch] = useReducer(reducer, mockTodo);
+
+  const idRef = useRef(3);
 
   // 할일 생성 함수
   const createTodo = (content) => {
-    const newItem = {
-      id: idRef.current,
-      isDone: false,
-      content,
-      createdDate: new Date().getTime(),
-    };
-    setTodo([...todo, newItem]);
+    dispatch({
+      type: "CREATE",
+      newItem: {
+        id: idRef.current,
+        isDone: false,
+        content,
+        createdDate: new Date().getTime(),
+      },
+    });
     idRef.current++;
   };
 
   // todo의 item의 id와 같으면 isDone 변경하는 로직 (할일 수정 함수)
   const updateTodo = (targetId) => {
-    setTodo(
-      todo.map((item) => {
-        return item.id === targetId ? { ...item, isDone: !item.isDone } : item;
-      })
-    );
+    dispatch({
+      type: "UPDATE",
+      targetId,
+    });
   };
 
   // 할일 삭제
   const deleteTodo = (targetId) => {
-    const newTodo = todo.filter((item) => item.id !== targetId);
-    setTodo(newTodo);
+    dispatch({
+      type: "DELETE",
+      targetId,
+    });
   };
 
   return (
     <>
-      <TestComp />
+      {/* <TestComp /> */}
       <div className="mx-40 px-10 py-10 border flex flex-col gap-12">
         <Header />
         <TodoEditor onCreateTodo={createTodo} />
